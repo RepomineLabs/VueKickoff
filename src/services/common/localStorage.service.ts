864bc043055f0d0dd45587fc6fc3cdf7793e9efe
oclk <oclk@outlook.com.tr>
2022-10-15 22:10:02 +0300

    feat: Added setItem method in Local Storage Service

--- src/services/common/localStorage.service.ts
+++ src/services/common/localStorage.service.ts
@@ -1,5 +1,12 @@
 class LocalStorageService {
     constructor() {
-        
+
+    }
+
+    setItem = (key: string, value: any) => {
+        if (value) {
+            let byteValue = btoa(unescape(encodeURIComponent(value)))
+            localStorage.setItem(key, byteValue)
+        }
     }
 }
