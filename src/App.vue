03d603ab76814653f767c3cb4cc8a57110f28e05
oclk <oclk@outlook.com.tr>
2022-10-15 22:36:42 +0300

    refactor: Redesing comps and views places

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './common/components/HelloWorld.vue'
 </script>
 
 <template>
