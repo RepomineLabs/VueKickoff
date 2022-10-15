041f1ab276f8f6848cbbec177674570c901ff142
oclk <oclk@outlook.com.tr>
2022-10-15 22:13:16 +0300

    feat: Added removeItems to Local Storage Service

--- src/services/common/localStorage.service.ts
+++ src/services/common/localStorage.service.ts
@@ -24,4 +24,10 @@ class LocalStorageService {
     removeItem = (key: string) => {
         localStorage.removeItem(key)
     }
+
+    removeItems = (keys: string[]) => {
+        for (let i = 0; i < keys.length; i++) {
+            localStorage.removeItem(keys[i])
+        }
+    }
 }
