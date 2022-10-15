5857f3cbf3b6967cd304167fb651752555db12bc
oclk <oclk@outlook.com.tr>
2022-10-15 22:47:56 +0300

    refactor: Removed  view folder and created pages and layouts

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/pages/HomeView.vue')
+      component: () => import('../pages/HomeView.vue')
     },
   ]
 })
