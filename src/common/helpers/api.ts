0e218014090b0fa62e5cb518f887208a50f5698b
oclk <oclk@outlook.com.tr>
2022-10-16 16:45:52 +0300

    feat: Added request interceptor configurations in api.ts

--- src/common/helpers/api.ts
+++ src/common/helpers/api.ts
@@ -1,4 +1,8 @@
 import axios, { type AxiosInstance, type AxiosRequestConfig, type AxiosResponse, type CancelToken } from 'axios'
+import LocalStorageService from '../services/localStorage.service'
+
+const LOCAL_STORAGE_ACCESS_TOKEN = 'AccessToken'
+const LOCAL_STORAGE_REFRESH_TOKEN = 'RefreshToken'
 
 abstract class Api {
     instance: AxiosInstance
@@ -8,5 +12,26 @@ abstract class Api {
         this.instance = axios.create({
             baseURL,
         })
+
+        // Activate request interceptor
+        this._initReqInterceptor()
     }
+
+    // Request Interceptor
+    _initReqInterceptor = () => {
+        this.instance.interceptors.request.use(
+            this._handleReq,
+            this._handleReqError,
+        )
+    }
+
+    // Calling in the interceptor > request.use
+    _handleReq = (config: AxiosRequestConfig): AxiosRequestConfig => {
+        config.headers['Authorization'] = 'Bearer ' + LocalStorageService.getItem(LOCAL_STORAGE_ACCESS_TOKEN)
+
+        return config
+    }
+
+    // Calling in the interceptor > request.use
+    _handleReqError = (error) => Promise.reject(error)
 }
