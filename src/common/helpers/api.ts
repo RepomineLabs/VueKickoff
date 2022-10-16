a945563166f39d16ee83f9e2683dad06314a19ff
oclk <oclk@outlook.com.tr>
2022-10-16 22:14:56 +0300

    feat: Added _handleRespError to api.ts

--- src/common/helpers/api.ts
+++ src/common/helpers/api.ts
@@ -1,4 +1,4 @@
-import axios, { type AxiosInstance, type AxiosRequestConfig, type AxiosResponse, type CancelToken } from 'axios'
+import axios, { type AxiosInstance, type AxiosRequestConfig, type AxiosResponse, type CancelToken, type AxiosError } from 'axios'
 import LocalStorageService from '../services/localStorage.service'
 
 const LOCAL_STORAGE_ACCESS_TOKEN = 'AccessToken'
@@ -53,6 +53,57 @@ abstract class Api {
             return Promise.reject(response.data.ErrorCode)
         } else {
             return Promise.resolve(response)
+        }
+    }
+
+    // Calling in the interceptor > response.use
+    // Debug error and starts the refresh token process if necessary.
+    _handleRespError = (error: any) => {
+        const config = error.config
+        if (config && config.url !== ".../Login" && error.response) {
+            if (error.response?.status === 401 && !config._retry) {
+                config._retry = true;
+                return new Promise((resolve, reject) => {
+                    const AccessToken = LocalStorageService.getItem(LOCAL_STORAGE_ACCESS_TOKEN)
+                    const RefreshToken = LocalStorageService.getItem(LOCAL_STORAGE_REFRESH_TOKEN)
+                    this.instance.post('.../RefreshAccessToken', { RefreshToken, AccessToken })
+                        .then((response: any) => {
+                            if (response.data.IsSucceed) {
+                                // 1) Store Auth to State
+
+                                // 2) Change Content-Type header
+                                if (config.headers) {
+                                    config.headers['Authorization'] = `Bearer ${LocalStorageService.getItem(LOCAL_STORAGE_ACCESS_TOKEN)}`
+                                }
+
+                                // 3) Return request object with axios
+                                this.instance(config).then(_response => {
+                                    resolve(_response);
+                                }).catch((err) => {
+                                    reject(err);
+                                })
+                            } else {
+                                // store.dispatch('auth/logout').then(() => {
+                                //     router.push('/auth/login')
+                                // })
+                            }
+                        })
+                        .catch(_error => {
+                            // store.dispatch('auth/logout').then(() => {
+                            //     router.push('/auth/login')
+                            // })
+                            return Promise.reject(_error)
+                        })
+                });
+
+            } else if (error.response.status === 401) {
+                // store.dispatch('auth/logout').then(() => {
+                //     router.push('/auth/login')
+                // })
+                return Promise.reject(error)
+            } else {
+                return Promise.reject(error)
+            }
         }
     }
 }
