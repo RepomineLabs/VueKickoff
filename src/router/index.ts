76efa233dab96bc1d49e1c85cf2a029306f8da1c
oclk <oclk@outlook.com.tr>
2022-10-16 22:46:34 +0300

    refactor: Chaged base route imp

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import DefaultLayout from '../views/layouts/DefaultLayout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: () => import('../views/pages/HomeView.vue')
+      name: '',
+      component: DefaultLayout,
+      children: [
+        {
+          path: 'login',
+          name: 'Login',
+          component: () => import('../views/pages/LoginView.vue')
+        },
+        {
+          path: 'register',
+          name: 'Register',
+          component: () => import('../views/pages/RegisterView.vue')
+        },
+        {
+          path: '/',
+          name: 'home',
+          component: () => import('../views/pages/HomeView.vue')
+        },
+      ]
     },
   ]
 })
